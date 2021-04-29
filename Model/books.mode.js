870b5babf42335b03266cc2ad92edfb51ddd6e43
saket98 <saket.pawar98@gmail.com>
2021-04-29 11:24:10 +0530

    Get Post and Delete route added

--- Model/books.mode.js
+++ Model/books.mode.js
@@ -1,12 +1,12 @@
 const Mongoose = require("mongoose");
 
-const Book = new Mongoose.Schema({
+const BookSchema = new Mongoose.Schema({
 	title: { type: String, require: true },
-	auther: { type: String, require: true },
+	author: { type: String, require: true },
 	publisher: { type: String, require: true },
 	price: { type: Number, require: true },
 	publishing_year: { type: Number },
 	genre: { type: String },
 });
 
-module.exports = Book;
+module.exports = Mongoose.model("Book", BookSchema);;
--- Route/books.route.js
+++ Route/books.route.js
@@ -1,21 +1,59 @@
 const express = require("express");
 const route = express.Router();
-const Book = require("../Model/books.mode");
+const BookSchema = require("../Model/books.mode");
 
-route.get("/", async (req, res) => {
-	const books = Book.find();
-	res.status(200).send(books);
+route.get("/", async (req, res, next) => {
+	try {
+		const books = await BookSchema.find();
+		res.status(200).send(books);
+	} catch (error) {
+		next(error);
+	}
 });
 
-route.post("/", async (req, res) => {
-	const book = Book.find({ title: req.title, author: req.author, publishing_year: req.publishing_year });
+route.post("/", async (req, res, next) => {
+	try {
+		const book = await BookSchema.findOne({ $and: [{ title: req.body.title }, { author: req.body.author }, { publishing_year: req.body.publishing_year }] });
+		if (!book) {
+			const save_body = new BookSchema(req.body);
+			const book = await save_body.save();
+			res.status(201).send({
+				book,
+				message: "Review saved successfully.",
+			});
+		} else {
+			res.status(400).send({
+				message: "Book already exist",
+			});
+		}
+	} catch (error) {
+		next(error);
+	}
+});
 
-	if (!book) {
-		const save_book = await book.save();
-		res.status(201).send({
-			book,
-			message: "Review saved successfully.",
-		});
+route.delete("/:id", async (req, res, next) => {
+	try {
+		const bookId = req.params.id;
+		console.log(bookId);
+		const book = await BookSchema.findByIdAndDelete(bookId);
+		console.log(book);
+		if(book) res.status(200).send({message: `Book with id ${bookId} has been deleted`});
+		else res.status(400).send({ message: "Wrong ID provided",});
+	} catch (error) {
+		next(error);
+	}
+});
+
+route.put("/:id", async (req, res, next) => {
+	try {
+		const bookId = req.params.id;
+		console.log(bookId);
+		const book = await BookSchema.findByIdAndDelete(bookId);
+		console.log(book);
+		if(book) res.status(200).send({message: `Book with id ${bookId} has been deleted`});
+		else res.status(400).send({ message: "Wrong ID provided",});
+	} catch (error) {
+		next(error);
 	}
 });
 
